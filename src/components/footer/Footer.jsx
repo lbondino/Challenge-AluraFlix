9c2a3f1e8dcd34ed7edf6c054298a73d289347eb
lbondino <lbondino@gmail.com>
2024-07-15 16:39:18 -0300

    subo challenge

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,90 @@
+import { styled } from 'styled-components';
+import { BsInstagram, BsLinkedin } from "react-icons/bs";
+import { FaSquareFacebook, FaXTwitter } from "react-icons/fa6";
+import './Footer.css';
+import logo from '../../assets/logo.png';
+
+const StyledFooter = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-black);
+    padding: 2rem;
+    box-sizing: border-box;
+    border-top: 4px solid var(--color-blue);
+    box-shadow: 0 -10px 15px 0 var(--color-button-shadow);
+`;
+
+const IconContainer = styled.ul`
+    background-color: transparent;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+        background-color: var(--color-black);
+        display: inline-block;
+        //margin-right: 1.5rem;
+        padding: 0.5rem;
+    }
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        width: clamp(30%, 100%, 10.528rem); 
+        height: auto;
+    }
+`;
+
+const FooterText = styled.p`
+    background-color: transparent;
+    font-size: 13px;
+    color: var(--color-white-smoke);
+    margin: 0;
+    line-height: 1.4;
+    text-align: center;
+`;
+
+function Footer() {
+    return (
+        <StyledFooter className='container'>
+            <LogoContainer className='logo'>
+                <img src={logo} alt="Logo" />
+            </LogoContainer>
+            <IconContainer>
+                <li>
+                    <a href="https://www.facebook.com/liliana.bondino.77/">
+                    <FaSquareFacebook className='icons'/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://x.com/BondinoLily">
+                    <FaXTwitter className='icons'/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/lilybond1701/">
+                    <BsInstagram className='icons' />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/liliana-bondino/">
+                    <BsLinkedin className='icons' />
+                    </a>
+                </li>
+            </IconContainer>
+            <FooterText className='text'>
+                Desarrollado por Liliana Bondino<br />
+                Julio de 2024
+            </FooterText>
+        </StyledFooter>
+    );
+}
+
+export default Footer
---
+++ src/data/CategoryData.js
@@ -0,0 +1,20 @@
+
+const categoryData = [
+  {
+    id: 1,
+    name: "Películas Familiares",
+    primaryColor: "var(--color-familiar)",
+  },
+  {
+    id: 2,
+    name: "Ciencia Ficción",
+    primaryColor: "var(--color-cienciaficcion)",
+  },
+  {
+    id: 3,
+    name: "Terror",
+    primaryColor: "var(--color-terror)",
+  }
+];
+
+export default categoryData;
